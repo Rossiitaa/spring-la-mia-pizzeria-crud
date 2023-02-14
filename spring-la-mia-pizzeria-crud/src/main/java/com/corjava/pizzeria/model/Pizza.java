795bb652c0b6fe6c8045b1a2496c780b467c1147
Rossiitaa <rosario.scuotto@outlook.it>
2023-02-14 15:32:48 +0100

    Adds Pizza entity class

---
+++ spring-la-mia-pizzeria-crud/src/main/java/com/corjava/pizzeria/model/Pizza.java
@@ -0,0 +1,61 @@
+package com.corjava.pizzeria.model;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "pizze")
+public class Pizza {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String name;
+	private String description;
+	private BigDecimal price;
+	
+	@Column(name = "image", nullable = true)
+	private String imgURL;
+
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	public BigDecimal getPrice() {
+		return price;
+	}
+
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+
+	public String getImgURL() {
+		return imgURL;
+	}
+
+	public void setImgURL(String imgURL) {
+		this.imgURL = imgURL;
+	}
+}
