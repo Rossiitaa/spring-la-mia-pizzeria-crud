4ad89ad591959c81d3ed66422841947f64310d1e
Rossiitaa <rosario.scuotto@outlook.it>
2023-02-20 12:16:31 +0100

    creation, saving and validation

--- spring-la-mia-pizzeria-crud/src/main/java/com/corjava/pizzeria/controller/PizzaController.java
+++ spring-la-mia-pizzeria-crud/src/main/java/com/corjava/pizzeria/controller/PizzaController.java
@@ -5,16 +5,21 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.corjava.pizzeria.model.Pizza;
 import com.corjava.pizzeria.repository.PizzaRepo;
 
+import jakarta.validation.Valid;
+
 @Controller
-@RequestMapping("/pizze")
+@RequestMapping("/")
 public class PizzaController {
 	@Autowired
 	PizzaRepo pizzaRepo;
@@ -25,17 +30,38 @@ public class PizzaController {
 		if (keyword == null) {
 			elencoPizze = pizzaRepo.findAll();
 		} else {
-			elencoPizze = pizzaRepo.findByNomeLike("%" + keyword + "%");
+			elencoPizze = pizzaRepo.findByNameLike("%" + keyword + "%");
 		}
 
-		model.addAttribute("elencoPizze", elencoPizze);
+		model.addAttribute("pizze", elencoPizze);
 		return "index";
 	}
 
-	@GetMapping("/{id}")
-	public String dettaglioPizza(@PathVariable("id") Integer id, Model model) {
-		Pizza singolaPizza = pizzaRepo.getReferenceById(id);
-		model.addAttribute("pizza", singolaPizza);
+	@GetMapping("/details/{id}")
+	public String details(@PathVariable("id") Integer id, Model model) {
+		Pizza pizza = pizzaRepo.getReferenceById(id);
+		model.addAttribute("pizza", pizza);
 		return "details";
 	}
+
+	@GetMapping("/create")
+	public String create(Model model) {
+		Pizza pizza = new Pizza();
+		model.addAttribute("pizza", pizza);
+		return "create";
+	}
+	
+	@PostMapping("/create")
+	public String store(
+		@Valid	@ModelAttribute("pizza") Pizza formPizza,
+		BindingResult bindingResult,
+		Model model) {
+		if(bindingResult.hasErrors()) 
+			return "create";
+		
+		pizzaRepo.save(formPizza);
+		return "redirect:/";
+	}	
+	
+	
 }
